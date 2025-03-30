e9c445085f62cc666dee7573314ad69211400ad6
ET2319013 <2319013@vum.bg>
2025-03-30 16:50:35 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        axios.get("http://localhost:5140/api/hello")
+            .then(response => setMessage(response.data.message))
+            .catch(error => console.error("Error fetching message:", error));
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <h1>React + .NET 8 API</h1>
+      <p>{message || "Loading..."}</p>
     </div>
   );
 }
